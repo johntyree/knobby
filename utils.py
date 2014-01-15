0a93c762bc1bd0f79cb080fc455640ded9fa22ec
John Tyree <johntyree@gmail.com>
2014-01-15 15:53:58 -0600

    Add binary diff utility functions

--- utils.py
+++ utils.py
@@ -34,3 +34,18 @@ def chunks_of(sz, buf):
         data += bytes(sz - len(data))
         yield data
         data = buf.read(sz)
+
+
+def as_binary(long_):
+    return "{:0= 64b}".format(long_)
+
+
+def diff_binary(a, b):
+    s1 = as_binary(a)
+    s2 = as_binary(b)
+    return string_diff(s1, s2)
+
+
+def string_diff(s1, s2):
+    chars = [' ' if a == b else '|' for a, b in zip(s1, s2)]
+    return ''.join(chars)
