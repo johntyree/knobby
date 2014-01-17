d2084d4ad3ea8db813fcd6f509b428790c0e8680
John Tyree <johntyree@gmail.com>
2014-01-17 16:09:13 -0600

    Rename Event attributes to `id` and `mask`

--- knobby/event.py
+++ knobby/event.py
@@ -26,14 +26,14 @@ class Event(object):
     BUTTON_UP_ON  = 0x0
     BUTTON_UP_OFF = 0x0
 
-    def __init__(self, seconds, microseconds, second, thoid):
+    def __init__(self, seconds, microseconds, event_id, event_mask):
         self.time = seconds + (microseconds * 1e-6)
-        self.b = second
-        self.c = thoid
+        self.id = event_id
+        self.mask = event_mask
         self._hex_print = True
 
     def __repr__(self):
-        fmt = """Event(time={time}, b={b}, c={c})"""
+        fmt = """Event(time={time}, id={id}, mask={mask})"""
         return fmt.format(**vars(self))
 
     def __str__(self):
@@ -43,6 +43,6 @@ class Event(object):
             fmt = as_binary
         d = []
         d.append(repr(self))
-        d.append(fmt(self.b))
-        d.append(fmt(self.c))
+        d.append(fmt(self.id))
+        d.append(fmt(self.mask))
         return '{:50} {} {}'.format(*d)
