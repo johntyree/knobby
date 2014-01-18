024677ff1069ba624b6b3d00bfa5c94276c03cfa
John Tyree <johntyree@gmail.com>
2014-01-18 17:38:01 -0600

    Update `knobby.main` to use EventHandler

--- knobby/main.py
+++ knobby/main.py
@@ -3,50 +3,28 @@
 
 from __future__ import division, print_function
 
-import itertools as it
 import optparse
 import sys
 
-from .event import Event
-from .utils import struct_stream
+from .event import EventHandler
 
 
 def parse_args(argv=sys.argv):
     o = optparse.OptionParser()
     o.add_option('-n', '--num-events', dest='n', type=int)
-    o.add_option('-f', '--input-file', default='-', dest='filename',
-                 help="The event file.")
+    o.add_option('-f', '--input-file', default='/dev/powermate',
+                 dest='filename', help="The PowerMate character device.")
     opts, args = o.parse_args(argv)
+    if opts.filename == '-':
+        opts.filename = '/dev/stdin'
     return opts, args
 
 
-def process(fin, callback=print, n=None):
-    """Read and process a stream of events.
-
-    n : int
-        Limit processing to `n` events.
-
-    callback : callable
-        callback is called on each event.
-
-    """
-    chunks = struct_stream(Event, fin)
-    if n is not None:
-        chunks = it.islice(chunks, n)
-    for event in chunks:
-        ret = callback(event)
-        if not ret:
-            return ret
-
-
-def main():
-    """Run main."""
-    opts, _ =parse_args()
-    if opts.filename != '-':
-        with open(opts.filename, 'rb') as fin:
-            process(fin, opts.n)
-    else:
-        process(sys.stdin, opts.n)
+def main(handler=None):
+    """Run the given `EventHandler` or a default one if None given."""
+    opts, _ = parse_args()
+    handler = handler or EventHandler()
+    handler.process()
     return 0
 
 
