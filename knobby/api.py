8ff3a7d1212df5e0816a0033882dc20827135a2c
John Tyree <johntyree@gmail.com>
2014-01-18 18:10:44 -0600

    Remove volume control example from knobby.api

--- knobby/api.py
+++ knobby/api.py
@@ -3,11 +3,10 @@
 
 
 __all__ = [
-    'volume_control_handler',
     'EventHandler',
     'main',
 ]
 
+
 from .main import main
 from .event import EventHandler
-from .handlers.volume_control import volume_control_handler
