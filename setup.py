2a829c3a091a95571e2193190276db8d3b4ee3af
John Tyree <johntyree@gmail.com>
2015-08-18 22:52:48 -0500

    Cleanup whitespace in setup.py

--- setup.py
+++ setup.py
@@ -3,20 +3,24 @@
 
 from setuptools import setup
 
-setup(name='knobby',
-      version='0.0.1',
-      packages=['knobby'],
-      author='John Tyree',
-      author_email='johntyree@gmail.com',
-      license='GPL3+',
-      url='http://github.com/johntyree/knobby',
-      description="Interface for the Griffin PowerMate",
-      keywords="PowerMate wheel usb",
-      long_description=open('README.md').read(),
-      classifiers=[
-          "Development Status :: 3 - Alpha",
-          "License :: OSI Approved :: "
-          "GNU General Public License v3 or later (GPLv3+)",
-          "Topic :: Utilities",  #FIXME
-      ],
-      )
+setup(
+    name='knobby',
+    version='0.0.1',
+    packages=['knobby'],
+    author='John Tyree',
+    author_email='johntyree@gmail.com',
+    license='GPL3+',
+    url='http://github.com/johntyree/knobby',
+    description="Interface for the Griffin PowerMate",
+    keywords="PowerMate wheel usb",
+    long_description=open('README.md').read(),
+    entry_points={
+        'console_scripts': ['knobby = knobby.handlers.fart:main'],
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: "
+        "GNU General Public License v3 or later (GPLv3+)",
+        "Topic :: Utilities",  # FIXME
+    ],
+)
